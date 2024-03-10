f46c6ff0e81fdf6ac99e6e4b9a4a5fdfb02d21dd
Doctor <thirvondukr@gmail.com>
2024-03-10 10:31:40 +0300

    Mask graphql errors

--- src/app/adapters/graphql/schema.py
+++ src/app/adapters/graphql/schema.py
@@ -1,8 +1,10 @@
 import strawberry
 from aioinject.ext.strawberry import AioInjectExtension
+from graphql import GraphQLError
 from strawberry import Schema
 from strawberry.enum import EnumDefinition, EnumValue
-from strawberry.extensions import ParserCache, ValidationCache
+from strawberry.exceptions import StrawberryGraphQLError
+from strawberry.extensions import MaskErrors, ParserCache, ValidationCache
 from strawberry.schema.config import StrawberryConfig
 from strawberry.schema.name_converter import NameConverter
 from strawberry.tools import merge_types
@@ -16,6 +18,7 @@ from app.adapters.graphql.apps.manga.mutation import MangaMutationsGQL
 from app.adapters.graphql.apps.manga.query import MangaQuery
 from app.adapters.graphql.apps.tags.query import TagsQueryGQL
 from app.adapters.graphql.apps.users.query import UserQuery
+from app.adapters.graphql.extensions import IsAuthenticated
 from app.core.di import create_container
 
 Query = merge_types(
@@ -56,12 +59,21 @@ class CustomNameConverter(NameConverter):
         return enum_value.name.upper()
 
 
+def _should_mask_error(error: GraphQLError) -> bool:
+    if not isinstance(
+        error.original_error, StrawberryGraphQLError
+    ):  # pragma: no cover
+        return True
+    return error.original_error.extensions != IsAuthenticated.error_extensions
+
+
 schema = Schema(
     query=Query,
     mutation=Mutation,
     extensions=[
         ParserCache(maxsize=128),
         ValidationCache(maxsize=128),
+        MaskErrors(_should_mask_error),
         AioInjectExtension(container=create_container()),
     ],
     config=StrawberryConfig(name_converter=CustomNameConverter()),
