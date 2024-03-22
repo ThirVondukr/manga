3da4f0b2cbffcabd03967bfb06184bf7ff5fcb70
Doctor <thirvondukr@gmail.com>
2024-03-22 22:02:23 +0300

    Add prefix to app settings

--- src/app/settings.py
+++ src/app/settings.py
@@ -69,6 +69,8 @@ class S3Settings(BaseSettings):
 
 
 class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="app_")
+
     cors_allow_origins: list[str] = []
     cors_allow_methods: list[str] = ["*"]
     cors_allow_headers: list[str] = ["authorization"]
